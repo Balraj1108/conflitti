318bbfa45afcdaba290ad059d19e4014892a7223
RobertoEsposito02 <robiespo02@libero.it>
2022-12-01 15:00:09 +0100

    risolto conflitto xxxx

--- src/conflitti/Test.java
+++ src/conflitti/Test.java
@@ -5,11 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
 		System.out.println("ciao buon natale balraj");
-=======
-		System.out.println("ciao buon natale casa stai");
->>>>>>> b685f6a96de6e551a6576955422c8ff3cebd7afc
 		System.out.println("ciao stai brutto demonio");
 		System.out.println("ciao come lol maledetto");
 
