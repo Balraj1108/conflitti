62ad50a7b7424c05585f34f38d6f401a9a558328
Balraj1108 <balrajsingh1108@gmail.com>
2022-12-01 14:05:33 +0100

    primo commit

---
+++ src/conflitti/Test.java
@@ -0,0 +1,14 @@
+package conflitti;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("ciao buon natale");
+		System.out.println("ciao stai natale");
+		System.out.println("ciao come lol");
+
+	}
+
+}
