b685f6a96de6e551a6576955422c8ff3cebd7afc
Balraj1108 <balrajsingh1108@gmail.com>
2022-12-01 14:43:07 +0100

    modifica frase

--- src/conflitti/Test.java
+++ src/conflitti/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ciao buon natale");
+		System.out.println("ciao buon natale casa stai");
 		System.out.println("ciao stai brutto demonio");
 		System.out.println("ciao come lol maledetto");
 
